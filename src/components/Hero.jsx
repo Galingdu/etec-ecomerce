56f3a1a6775ca8426c2e76faf6d36dd66e62942b
Galingdu <henglanhzaa09.12.2021@gmail.com>
2025-06-20 00:27:33 +0700

    aos fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Hero() {
   return ( 
-    <div className='hero-bg lg:px-[120px]' data-aos="fade-up">
+    <div className='hero-bg lg:px-[120px]'>
         <div className='p-5 w-[85%] lg:w-[40%] md:w-[55%] py-30 lg:py-45'>
           <h2 className='text-4xl font-bold text-white md:text-6xl'><Typing /></h2>
           <p className='text-gray-200 my-10'>Shopping is a bit of a relaxing hobby for me, which is sometimes troubling for the bank balance.</p>
